51ce830e44defc0615261f94a42e9b7bc156c4f4
Winggo Tse <winggo.tse@gmail.com>
2020-11-11 15:13:05 -0800

    add babel devDependencies

--- packages/react-signature-frame/webpack.config.js
+++ packages/react-signature-frame/webpack.config.js
@@ -1,23 +1,37 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    path: path.resolve('lib'),
+    path: path.resolve(__dirname, 'lib'),
     filename: 'index.js',
     libraryTarget: 'commonjs2',
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
       {
         test: /\.css$/,
+        exclude: /node_modules/,
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src'),
+      __dirname,
+      'node_modules',
+    ],
+    extensions: ['*', '.js', '.jsx'],
   },
 }
