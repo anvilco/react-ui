872339ae335fb94728532103124a9a0ff190d47b
Winggo Tse <winggo.tse@gmail.com>
2020-11-18 11:23:59 -0800

    try import frame with relative path

--- packages/react-signature-modal/src/index.js
+++ packages/react-signature-modal/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ReactModal from 'react-modal'
-import AnvilSignatureFrame from '@anvilco/react-signature-frame'
+import AnvilSignatureFrame from '../../react-signature-frame/src/index.js'
 import DeleteIcon from './components/DeleteIcon'
 import './index.css'
 
