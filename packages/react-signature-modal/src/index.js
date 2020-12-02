050ffcc1760df814ec0d2eb919d9b8ae8f21c949
Winggo Tse <winggo.tse@gmail.com>
2020-12-02 13:10:02 -0800

    Update index.js

--- packages/react-signature-modal/src/index.js
+++ packages/react-signature-modal/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ReactModal from 'react-modal'
+
 import AnvilSignatureFrame from '../../react-signature-frame/src/index.js'
 import IconClose from './components/IconClose.js'
 
