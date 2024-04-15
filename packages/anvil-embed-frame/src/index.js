88fb11d3d99b3dd0e4777cf6c708f2ee50b8f75a
patapat <patapat@gmail.com>
2024-04-15 13:45:18 -0700

    Cleanup

--- packages/anvil-embed-frame/src/index.js
+++ packages/anvil-embed-frame/src/index.js
@@ -1,8 +1,17 @@
-import React, {  useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
-const AnvilEmbedFrame = ({ iframeURL, onEvent, anvilURL, scroll, style, iframeRef, ...others }) => {
-  const ref = iframeRef || useRef(null)
+const AnvilEmbedFrame = ( props ) => {
+const {
+  iframeURL,
+  onEvent = () => {},
+  anvilURL = 'https://app.useanvil.com',
+  scroll,
+  style,
+  iframeRef,
+  ...others
+} = props
+const ref = iframeRef || useRef(null)
 
   useEffect(() => {
     const handleEvent = ({ origin, data }) => {
@@ -13,12 +22,12 @@ const AnvilEmbedFrame = ({ iframeURL, onEvent, anvilURL, scroll, style, iframeRe
     }
 
     if (scroll) ref.current?.scrollIntoView({ behavior: scroll })
-    window.addEventListener('message', handleEvent);
+    window.addEventListener('message', handleEvent)
 
     return () => {
-      window.removeEventListener('message', handleEvent);
-    };
-  }, [anvilURL, onEvent, scroll]);
+      window.removeEventListener('message', handleEvent)
+    }
+  }, [anvilURL, onEvent, scroll])
 
   return (
     <iframe
@@ -32,11 +41,6 @@ const AnvilEmbedFrame = ({ iframeURL, onEvent, anvilURL, scroll, style, iframeRe
       <p id="anvil-iframe-warning">Your browser does not support iframes.</p>
     </iframe>
   )
-}
-
-AnvilEmbedFrame.defaultProps = {
-  onEvent: () => {},
-  anvilURL: 'https://app.useanvil.com',
 }
 
 AnvilEmbedFrame.propTypes = {
